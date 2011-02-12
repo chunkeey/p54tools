0351a528628d5a25730bdcacf4ac0eed122c3264
Christian Lamparter <chunkeey@googlemail.com>
2011-02-12 12:06:35 +0100

    p54tools: fix *64 type redefinitions
    Signed-off-by: Christian Lamparter <chunkeey@googlemail.com>

--- include/types.h
+++ include/types.h
@@ -28,7 +28,6 @@
 
 typedef uint16_t	__le16;
 typedef uint32_t	__le32;
-typedef uint64_t	__le64;
 
 typedef uint8_t		u8;
 typedef uint8_t		__u8;
@@ -36,16 +35,12 @@ typedef uint16_t	u16;
 typedef uint16_t	__u16;
 typedef uint32_t	u32;
 typedef uint32_t	__u32;
-typedef uint64_t	u64;
-typedef uint64_t	__u64;
 typedef int8_t		s8;
 typedef int8_t		__s8;
 typedef int16_t		s16;
 typedef int16_t		__s16;
 typedef int32_t		s32;
 typedef int32_t		__s32;
-typedef int64_t		s64;
-typedef int64_t		__s64;
 
 #define cpu_to_le16(x) ((__le16)(uint16_t)(x))
 #define le16_to_cpu(x) ((uint16_t)(__le16)(x))
